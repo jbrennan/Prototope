f872653a77935f52847529f614546ac1c637e999
Jason Brennan <i.jasonbrennan@gmail.com>
2017-07-26 18:41:15 -0400

    Magnification stuff

--- Prototope/ScrollLayerMac.swift
+++ Prototope/ScrollLayerMac.swift
@@ -15,10 +15,24 @@ open class ScrollLayer: Layer {
 	
 	/** Create a layer with an optional parent layer and name. */
 	public init(parent: Layer? = nil, name: String? = nil) {
-		self.documentView = FlippedView(frame: CGRect())
+		documentView = FlippedView(frame: CGRect())
+		notificationHandler = ScrollViewDelegate()
+		
 		super.init(parent: parent, name: name, viewClass: SystemScrollView.self)
 		scrollView.documentView = documentView
-//		scrollView.delegate = scrollViewDelegate
+		
+		scrollView.allowsMagnification = true
+		
+		NotificationCenter.default.addObserver(
+			notificationHandler,
+			selector: #selector(ScrollViewDelegate.scrollViewDidScroll(notification:)),
+			name: NSNotification.Name.NSScrollViewDidLiveScroll, object: nil
+		)
+		NotificationCenter.default.addObserver(
+			notificationHandler,
+			selector: #selector(ScrollViewDelegate.scrollViewDidEndLiveMagnification(notification:)),
+			name: NSNotification.Name.NSScrollViewDidEndLiveMagnify, object: nil
+		)
 	}
 	
 	deinit {
@@ -29,6 +43,7 @@ open class ScrollLayer: Layer {
 		return self.view as! SystemScrollView
 	}
 	private let documentView: SystemView
+	private let notificationHandler: ScrollViewDelegate
 	
 	// This causes ScrollLayer's sublayers to be added to the scroll view's documentView, so they can be scrolled.
 	override var childHostingView: SystemView? {
@@ -68,6 +83,30 @@ open class ScrollLayer: Layer {
 		set { self.scrollView.hasHorizontalScroller = newValue }
 	}
 	
+	/** Controls whether or not the scrollView supports magnification. Defaults to `false`. */
+	open var allowsMagnification: Bool {
+		get { return self.scrollView.allowsMagnification }
+		set { self.scrollView.allowsMagnification = newValue }
+	}
+	
+	/** Controls whether or not the horizontal scroll indicator shows on scroll. Defaults to `true`. */
+	open var magnification: Double {
+		get { return Double(self.scrollView.magnification) }
+		set { self.scrollView.magnification = CGFloat(newValue) }
+	}
+	
+	open var maximumMagnification: Double {
+		get { return Double(self.scrollView.maxMagnification) }
+		set { self.scrollView.maxMagnification = CGFloat(newValue) }
+	}
+	
+	open var minimumMagnification: Double {
+		get { return Double(self.scrollView.minMagnification) }
+		set { self.scrollView.minMagnification = CGFloat(newValue) }
+	}
+	
+
+	
 //	/** This handler provides an opportunity to change the way a scroll layer decelerates.
 //	
 //	It will be called when the user lifts their finger from the scroll layer. The system will provide the user's velocity (in points per second) when they lifted their finger, along with a computed deceleration target (i.e. the point where the scroll view will stop decelerating). If you specify a non-nil handler, the point you return from this handler will be used as the final deceleration target for a decelerating scroll layer. You can return the original deceleration target if you don't need to modify it. **/
@@ -77,12 +116,19 @@ open class ScrollLayer: Layer {
 //	}
 //	
 //	
-//	/** This handler is called when the scrollView scrolls. */
-//	open var didScrollHandler: (() -> ())? {
-//		didSet {
-//			self.scrollViewDelegate.didScrollHandler = didScrollHandler
-//		}
-//	}
+	/** This handler is called when the scrollView scrolls. */
+	open var didScrollHandler: (() -> ())? {
+		didSet {
+			notificationHandler.didScrollHandler = didScrollHandler
+		}
+	}
+	
+	/** This handler is called when the scrollView magnifies. */
+	open var didMagnifyHandler: (() -> ())? {
+		didSet {
+			notificationHandler.didEndMagnifyingHandler = didMagnifyHandler
+		}
+	}
 	
 	
 	// MARK: - Methods
@@ -110,21 +156,17 @@ open class ScrollLayer: Layer {
 	}
 	
 	
-//	@objc fileprivate class ScrollViewDelegate: NSObject, UIScrollViewDelegate {
+	@objc fileprivate class ScrollViewDelegate: NSObject {
 //		var decelerationRetargetingHandler: ((_ velocity: Point, _ decelerationTarget: Point) -> Point)?
-//		var didScrollHandler: (() -> ())?
-//		
-//		@objc func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-//			if let decelerationRetargetingHandler = decelerationRetargetingHandler {
-//				let newTargetContentOffset = decelerationRetargetingHandler(Point(velocity), Point(targetContentOffset.pointee))
-//				targetContentOffset.pointee = CGPoint(newTargetContentOffset)
-//			}
-//		}
-//		
-//		@objc func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//			if let didScrollHandler = self.didScrollHandler {
-//				didScrollHandler()
-//			}
-//		}
-//	}
+		var didScrollHandler: (() -> ())?
+		var didEndMagnifyingHandler: (() -> ())?
+		
+		func scrollViewDidScroll(notification: NSNotification) {
+			didScrollHandler?()
+		}
+		
+		func scrollViewDidEndLiveMagnification(notification: NSNotification) {
+			didEndMagnifyingHandler?()
+		}
+	}
 }
