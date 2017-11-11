372a08498eb9418b701a7e885e45ef82821c416d
Jason Brennan <i.jasonbrennan@gmail.com>
2017-11-11 16:06:44 -0500

    Adding some cursor selections.

--- Prototope/Cursor.swift
+++ Prototope/Cursor.swift
@@ -6,11 +6,28 @@
 //  Copyright © 2017 Jason Brennan. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 import CoreGraphics
 
 open class Cursor {
 	open static func moveTo(screenPosition: Point) {
 		CGWarpMouseCursorPosition(CGPoint(screenPosition))
+	}
+	
+	open static func set(cursorAppearance: Appearance) {
+		cursorAppearance.nsCursor.set()
+	}
+	
+	public enum Appearance {
+		case arrow
+		case crosshair
+		// case image(Image)
+		
+		var nsCursor: NSCursor {
+			switch self {
+			case .arrow: return NSCursor.arrow()
+			case .crosshair: return NSCursor.crosshair()
+			}
+		}
 	}
 }
