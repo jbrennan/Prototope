93db8c1b55f14201db90c527f49dce94cc64da24
Jason Brennan <i.jasonbrennan@gmail.com>
2020-08-02 17:17:24 -0400

    TextEditorLayer now has some editing callbacks.

--- Prototope/Layers/TextEditorLayer.swift
+++ Prototope/Layers/TextEditorLayer.swift
@@ -10,11 +10,30 @@ import Cocoa
 
 /// Wraps `NSTextView`
 open class TextEditorLayer: Layer {
+	
+	private let notificationHandler = NotificationHandler()
+	
+	open var textDidBeginEditingHandler: VoidHandler? {
+		get { return notificationHandler.textDidBeginEditingHandler }
+		set { notificationHandler.textDidBeginEditingHandler = newValue }
+	}
+	
+	open var textDidChangeHandler: VoidHandler? {
+		get { return notificationHandler.textDidChangeHandler }
+		set { notificationHandler.textDidChangeHandler = newValue }
+	}
+	
+	open var textDidEndEditingHandler: VoidHandler? {
+		get { return notificationHandler.textDidEndEditingHandler }
+		set { notificationHandler.textDidEndEditingHandler = newValue }
+	}
+	
 	public init(parent: Layer? = nil, name: String? = nil) {
 		
 		super.init(parent: parent, name: name, viewClass: NSTextView.self)
 		
 		textView.wantsLayer = true
+		textView.delegate = notificationHandler
 	}
 	
 	open var textView: NSTextView {
@@ -29,5 +48,23 @@ open class TextEditorLayer: Layer {
 	open var font: SystemFont {
 		get { return textView.font ?? SystemFont.systemFont(ofSize: SystemFont.systemFontSize) }
 		set { textView.font = newValue }
+	}
+	
+	private class NotificationHandler: NSObject, NSTextViewDelegate {
+		var textDidBeginEditingHandler: VoidHandler?
+		var textDidChangeHandler: VoidHandler?
+		var textDidEndEditingHandler: VoidHandler?
+		
+		@objc func textDidBeginEditing(_ notification: Notification) {
+			textDidBeginEditingHandler?()
+		}
+		
+		@objc func textDidChange(_ notification: Notification) {
+			textDidChangeHandler?()
+		}
+		
+		@objc func textDidEndEditing(_ notification: Notification) {
+			textDidEndEditingHandler?()
+		}
 	}
 }
